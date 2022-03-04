8471c16b23abee39cc001be1b5767ebafcfa0d8d
Kshitij Amit Sabale <64065205+KshitijSabale@users.noreply.github.com>
2022-03-04 14:25:39 +0530

    plotpage

---
+++ frontend/src/pages/Plot/Plot.js
@@ -0,0 +1,119 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+
+const defaultValues = {
+    name: "",
+    parcels: null,
+    areaofparcel: null,
+};
+
+export default function Plot() {
+   
+    const [formValues, setFormValues] = useState(defaultValues);
+    
+    const handleNameChange = (e) => {
+        const { name, value } = e.target;
+        console.log(name+ " "+ value);
+        setFormValues({
+          ...formValues,
+          [name]: value,
+        });
+    };
+
+    const handleParcelsChange = (e) => {
+        const { name, value } = e.target;
+        console.log(name+ " "+ value);
+        setFormValues({
+          ...formValues,
+          [name]: value,
+        });
+    };
+
+    const handleAreaChange = (e) => {
+        const { name, value } = e.target;
+        console.log(name+ " "+ value);
+        setFormValues({
+          ...formValues,
+          [name]: value,
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formValues);
+        // idhar axios call ayega database mai update karne keleye 
+        // ( async()=>{
+        //     const plot = await axios.get('http://localhost:5000/createplot',{
+        //         withCredentials:true,
+        //     });
+        // })();
+        
+        // after adding new plot details 
+        // set the formvalues default again -- basically refresh form 
+
+
+        // so as we are doing axios call here i don't think we need use effect now . 
+    };
+
+    // useEffect(()=>{
+    // },[]);
+
+
+    return (        
+        <div  >
+            <div className='plot-form'> 
+           <form onSubmit={handleSubmit}>
+                <Grid container alignItems="center" justify="center" direction="column">
+                    <Grid item>
+                    <InputLabel shrink htmlFor="name-input"> Name Of The Plot </InputLabel>
+                        <TextField
+                            id="name-input"
+                            name="name"
+                            label="Name"
+                            type="text"
+                            value={formValues.name}
+                            onChange={handleNameChange}
+                        />
+                    </Grid>
+                    <br/>
+                    <Grid item>
+                    <InputLabel shrink htmlFor="noofparcels-input"> Number Of Parcels </InputLabel>
+                        <TextField
+                            id="noofparcel-input"
+                            name="parcels"
+                            label="No of Parcels"
+                            type="number"
+                            value={formValues.parcels}
+                            onChange={handleParcelsChange}
+                        />
+                    </Grid>
+                    <br/>
+                    <Grid item>
+                    <InputLabel shrink htmlFor="area-input"> Area Of the Plots </InputLabel>
+                        <TextField
+                            id="area-input"
+                            name="areaofparcel"
+                            label="Area of Parcel"
+                            type="number"
+                            value={formValues.areaofparcel}
+                            onChange={handleAreaChange}
+                        />
+                    </Grid>
+                    <br />
+                    <Button variant="contained" color="primary" type="submit">
+                        Submit
+                    </Button>
+                </Grid>
+            </form>
+            </div>
+        </div>
+    )
+}
