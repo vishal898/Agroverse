9f26b5377bd68cb8ae549b1d2a7f5972aee6428d
Sweety <53006089+sweet1125@users.noreply.github.com>
2022-03-26 01:55:48 +0530

    home card createed

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,15 +1,15 @@
 import Navbar from '../../Components/Navbar/Navbar';
+import OutlinedCard from '../../Components/HomeCard/HomeCard';
 // import Demo from '../../Components/Demo';
 const Home = () => {
   return (
     <>
      <Navbar/> 
-    <br /><br /><br />
+    <br />
+    <OutlinedCard/>
     <section className="hero-section">
-      <br /><br /><br /><br /><br /><br/><br/>
-
-      <p>HI</p>
-      {/* <Demo></Demo> */}
+      
+     
       
     </section>
   </>
