d7dd25982eb0b27c9c088e350cd9a3f2b68271ca
vishal898 <vishal.borse898@gmail.com>
2022-03-26 01:31:47 +0530

    crop

---
+++ backend/routes/demand-routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose');
+const { findOneAndUpdate } = require("../models/crop");
+const Crop = require('../models/crop');
+
+
+
+// get read 
+
+
+
+
+module.exports = router ;
