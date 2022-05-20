bcc50bbf1da5eaec1e288d00ae38070438b45cdf
Sweety <53006089+sweet1125@users.noreply.github.com>
2022-05-20 20:13:10 +0530

    homecard

--- frontend/src/Components/HomeCard/HomeCard.js
+++ frontend/src/Components/HomeCard/HomeCard.js
@@ -8,7 +8,7 @@ export default function OutlinedCard() {
     <div class="column">
       <div class="card1">
         <h3>Crop</h3>
-        <br/>
+        
         <p>This tab is to see added crop and add new crop</p>
         <p><button className="loginbutton" ><a style={{color:'white'}} href="/crop">Crop</a></button></p>
       </div>
@@ -17,7 +17,7 @@ export default function OutlinedCard() {
     <div class="column">
       <div class="card1">
         <h3>Plot</h3>
-        <br/>
+       
         <p>This tab is to see added plot and add new plot</p>
         <p><button className="loginbutton" ><a style={{color:'white'}} href="/plot">Plot</a></button></p>
       </div>
@@ -26,7 +26,7 @@ export default function OutlinedCard() {
     <div class="column">
       <div class="card1">
         <h3>Demand</h3>
-        <br/>
+      
         <p>This tab is to submit demand</p>
         <br/>
         <p><button className="loginbutton" ><a style={{color:'white'}} href="/demand">Demand</a></button></p>
@@ -37,7 +37,7 @@ export default function OutlinedCard() {
     <div class="column">
       <div class="card1">
         <h3>To-do</h3>
-        <br/>
+       
         <p>This tab is to see and select the daily task pushed on basis of demand</p>
        
         
